84202216a58d9120419e5c783d37b346be521db3
Sidia <sidia.reis1@gmail.com>
2022-02-16 20:43:24 -0800

    fix:Avoid empty fields and type fix

--- Model/Book.cs
+++ Model/Book.cs
@@ -1,13 +1,18 @@
 
 
+using System.ComponentModel.DataAnnotations;
+
 namespace library_app.Models
 {
     public class Book {
+        [Required]
         public string Title {get; set;}
+        [Required]
         public string Author {get; set;}
-        
-        public string Year {get; set;}
-        public string Genres {get; set;}
+        [Required]
+        public int Year {get; set;}
+        [Required]
+        public string Genre {get; set;}
         
 
     }
