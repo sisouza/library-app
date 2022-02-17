b04b3ee1867c95ebfeda62b9fc73de0d1ce38bb8
Sidia <sidia.reis1@gmail.com>
2022-02-16 20:27:24 -0800

    chore:Create Book Model

---
+++ Model/Book.cs
@@ -0,0 +1,14 @@
+
+
+namespace library_app.Models
+{
+    public class Book {
+        public string Title {get; set;}
+        public string Author {get; set;}
+        
+        public string Year {get; set;}
+        public string Genres {get; set;}
+        
+
+    }
+}
