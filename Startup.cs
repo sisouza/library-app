c8b2c80db9d860616716ad61b9d9c03e1add5a8b
Sidia <sidia.reis1@gmail.com>
2022-02-17 20:37:51 -0800

    "feat:Set automapper"

--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,9 @@ namespace library_app
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "library_app", Version = "v1" });
             });
+
+            //set auto mapper
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
