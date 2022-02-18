d91c8a3657ce5e76fdc18ccef3df278a99dd327b
Sidia <sidia.reis1@gmail.com>
2022-02-17 19:52:52 -0800

    feat:Add DbContext class

---
+++ Data/BookDbContext.cs
@@ -0,0 +1,17 @@
+using library_app.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace library_app.Data
+{
+
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+
+}
