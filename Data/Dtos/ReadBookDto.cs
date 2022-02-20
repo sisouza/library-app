3e6e22f9622284e8ca0bd1a9e7873059a0d8696d
Sidia <sidia.reis1@gmail.com>
2022-02-20 13:16:16 -0800

    feat:Set Dtos

---
+++ Data/Dtos/ReadBookDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using library_app.Models;
+
+namespace library_app.Data.Dtos
+{
+    public class ReadBookDto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title can not be empty")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Author can not be empty")]
+        public string Author { get; set; }
+        [Required(ErrorMessage = "Genre can not be empty")]
+        public string Genre { get; set; }
+        [Required(ErrorMessage = "Year can not be empty")]
+        public int Year { get; set; }
+    }
+}
