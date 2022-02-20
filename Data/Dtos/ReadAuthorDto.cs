a40261dabb82e6921db6f4d4430e0d75b4326300
Sidia <sidia.reis1@gmail.com>
2022-02-20 14:05:35 -0800

    feat:Create Author Dtos

---
+++ Data/Dtos/ReadAuthorDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using library_app.Models;
+
+namespace library_app.Data.Dtos
+{
+    public class ReadAuthorDto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "name can not be empty")]
+        public string Name { get; set; }
+
+    }
+}
---
+++ Data/Dtos/UpdateAuthorDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using library_app.Models;
+
+namespace library_app.Data.Dtos
+{
+    public class UpdateAuthorDto
+    {
+        [Required(ErrorMessage = "Name can not be empty")]
+        public string Name { get; set; }
+    }
+}
