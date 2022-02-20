f40fe7c147102445923c1912869a522fdb73c914
Sidia <sidia.reis1@gmail.com>
2022-02-20 13:16:35 -0800

    fix:Set profile

--- Profiles/BookProfile.cs
+++ Profiles/BookProfile.cs
@@ -1,15 +1,22 @@
 using AutoMapper;
 using library_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using library_app.Data.Dtos;
 
 namespace library_app.Profiles
 {
+
     public class BookProfile : Profile
     {
-        public class BookProfile()
+        public BookProfile()
         {
             CreateMap<CreateBookDto, Book>();
             CreateMap<Book, ReadBookDto>();
             CreateMap<UpdateBookDto, Book>();
         }
-}
+    }
+
 }
