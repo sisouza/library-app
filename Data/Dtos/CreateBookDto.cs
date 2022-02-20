62020903e425eb800e254f88f66c4da890940431
Sidia <sidia.reis1@gmail.com>
2022-02-20 15:13:03 -0800

    feat:Set fk in book dtos

--- Data/Dtos/CreateBookDto.cs
+++ Data/Dtos/CreateBookDto.cs
@@ -15,7 +15,7 @@ namespace library_app.Data.Dtos
         [Required(ErrorMessage = "Title can not be empty")]
         public string Title { get; set; }
         [Required(ErrorMessage = "Author can not be empty")]
-        public string Author { get; set; }
+        public int Author { get; set; }
         [Required(ErrorMessage = "Genre can not be empty")]
         public string Genre { get; set; }
         [Required(ErrorMessage = "Year can not be empty")]
--- Data/Dtos/ReadBookDto.cs
+++ Data/Dtos/ReadBookDto.cs
@@ -12,13 +12,12 @@ namespace library_app.Data.Dtos
         [Key]
         [Required]
         public int Id { get; set; }
-        [Required(ErrorMessage = "Title can not be empty")]
         public string Title { get; set; }
-        [Required(ErrorMessage = "Author can not be empty")]
-        public string Author { get; set; }
-        [Required(ErrorMessage = "Genre can not be empty")]
+
+        public Author Author { get; set; }
+
         public string Genre { get; set; }
-        [Required(ErrorMessage = "Year can not be empty")]
+
         public int Year { get; set; }
     }
 }
