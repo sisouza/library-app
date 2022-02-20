981dc3865c7e6a64218fe2245d32fae12d5a0ef2
Sidia <sidia.reis1@gmail.com>
2022-02-20 14:05:15 -0800

    feat:Set AuthorProfile

---
+++ Profiles/AuthorProfile.cs
@@ -0,0 +1,22 @@
+using AutoMapper;
+using library_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using library_app.Data.Dtos;
+
+namespace library_app.Profiles
+{
+
+    public class AuthorProfile : Profile
+    {
+        public AuthorProfile()
+        {
+            CreateMap<CreateAuthorDto, Author>();
+            CreateMap<Author, ReadAuthorDto>();
+            CreateMap<UpdateAuthorDto, Author>();
+        }
+    }
+
+}
