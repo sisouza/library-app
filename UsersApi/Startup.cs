08f22c94b0423c12e0bcbc2e4d3fe24bd2d87a31
Sidia <sidia.reis1@gmail.com>
2022-02-27 20:19:34 -0800

    chore:Set AutoMapper

--- UsersApi/Startup.cs
+++ UsersApi/Startup.cs
@@ -28,10 +28,7 @@ namespace UsersApi
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "UsersApi", Version = "v1" });
-            });
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
