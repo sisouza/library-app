32ccfbfa4cb387f7cdc230ed709711084884b284
Sidia <sidia.reis1@gmail.com>
2022-02-28 15:45:40 -0800

    fix:Dependencies References

--- UsersApi/Startup.cs
+++ UsersApi/Startup.cs
@@ -1,16 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using UsersApi.Data;
+using AutoMapper;
+
 
 namespace UsersApi
 {
@@ -26,9 +25,20 @@ namespace UsersApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UserDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("UserDbConnection")));
+            //set Identity
+            services.AddIdentity<IdentityUser<int>, IdentityRole<int>>()
+            //store data used for identity
+                .AddEntityFrameworkStores<UserDbContext>();
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "UsuariosApi", Version = "v1" });
+            });
+            services.AddScoped<UserService, UserService>();
             services.AddControllers();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,8 +47,6 @@ namespace UsersApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UsersApi v1"));
             }
 
             app.UseHttpsRedirection();
