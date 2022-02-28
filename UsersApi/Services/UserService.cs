c0564a05d032d6b211137f7250cf24e2f6df488d
Sidia <sidia.reis1@gmail.com>
2022-02-28 15:33:34 -0800

    feat:Add UserService

---
+++ UsersApi/Services/UserService.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentResults;
+using library_app.UsersApi.Data.Dtos;
+using Microsoft.AspNetCore.Identity;
+using UsersApi.Models;
+
+public class UserService
+    {
+        private IMapper _mapper;
+        private UserManager<IdentityUser<int>> _userManager;
+
+        public UserService(IMapper mapper, UserManager<IdentityUser<int>> userManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        public Result RegisterUser(CreateUserDto createDto)
+        {
+            User user = _mapper.Map<User>(createDto);
+            IdentityUser<int> userIdentity = _mapper.Map<IdentityUser<int>>(user);
+            //user that was mapped has a password
+            Task<IdentityResult> resultIdentity = _userManager.CreateAsync(userIdentity, createDto.Password);
+            if (resultIdentity.Result.Succeeded) return Result.Ok();
+            return Result.Fail("An error occur while registering User");
+
+        }
+    }
