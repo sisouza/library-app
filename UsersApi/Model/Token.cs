da9ba1c4ea0c5daa89c956be4153be75e6df8f51
Sidia <sidia.reis1@gmail.com>
2022-03-02 14:16:05 -0800

    feat:Create Token Model

---
+++ UsersApi/Model/Token.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersApi.Models
+{
+    public class Token
+    {
+        public Token(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
+    }
+}
