0ea88422964f3e41dcefbd397e963ee9bd6abc89
Sidia <sidia.reis1@gmail.com>
2022-03-07 21:00:11 -0800

    feat:Set secrets

--- UsersApi/Program.cs
+++ UsersApi/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using static System.Collections.Immutable.ImmutableArray<T>;
 
 namespace UsersApi
 {
@@ -21,6 +22,8 @@ namespace UsersApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureAppConfiguration((context, builder)=>
+                    builder.AddUserSecrets<Program>());
     }
 }
