8673127ee6a6a5041fa9b7a3e786d1206b5c4513
Sidia <sidia.reis1@gmail.com>
2022-03-19 19:36:16 -0700

    feat:Create reset password request

---
+++ UsersApi/Data/Requests/PasswordResetRequest.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UsersApi.Data.Requests
+{
+
+    public class PasswordResetRequest
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string RePassword { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Token { get; set; }
+    }
+
+
+}
+    
+}
