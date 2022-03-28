53e919766d9cfba920d3d70c122d2dd73fe235c4
Sidia <sidia.reis1@gmail.com>
2022-03-27 21:04:48 -0700

    feat: Set deafult admin user and role

--- UsersApi/Data/UserDbContext.cs
+++ UsersApi/Data/UserDbContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
 
 namespace UsersApi.Data
 {
@@ -8,6 +9,49 @@ namespace UsersApi.Data
     {
         public UserDbContext(DbContextOptions<UserDbContext> opt) : base(opt)
         {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //set default application admin user
+            IdentityUser<int> admin = new IdentityUser<int>
+            {
+                UserName = "admin",
+                NormalizedUserName = "ADMIN",
+                Email = "admin@admin.com",
+                NormalizedEmail = "ADMIN@ADMIN.com",
+                EmailConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                Id = 99999
+            };
+
+            //generate admin password
+            PasswordHasher<IdentityUser<int>> hasher = new PasswordHasher<IdentityUser<int>>();
+
+            admin.PasswordHash = hasher.HashPassword(admin, "Admin123!");
+
+            //entity with admin data
+            builder.Entity<IdentityUser<int>>().HasData(admin);
+
+            //create role
+            builder.Entity<IdentityRole<int>>().HasData(
+                new IdentityRole<int> {
+                    Id = 99999,
+                    Name = "admin",
+                    NormalizedName = "ADMIN"
+                }
+            );
+
+            //bind between user and role
+            builder.Entity<IdentityUserRole<int>>().HasData(
+                new IdentityUserRole<int> {
+                    RoleId = 99999,
+                    UserId = 99999
+                }
+            );
 
         }
     }
